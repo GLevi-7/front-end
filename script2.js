a7e776d02c874eb8c561e08df6390559bcf1bffd
GLevi-7 <giancarlo.siqueira@aluno.senai.br>
2026-08-10 18:11:41 -0300

    front-end2

---
+++ script2.js
@@ -0,0 +1,19 @@
+const inputUpload = document.getElementById('meu-upload')
+const nome = document.getElementById('nome')
+
+//const textoNome = document.getElementById('nome-arquivo');
+
+inputUpload.addEventListener('change', function(evento) {
+
+    //Pega o primeiro arquivo selecionado
+    const arquivo = evento.target.files[0]
+    
+    if (arquivo) {
+        //Obtém o nome da imagem
+        const nomeDaImagem = arquivo.name;
+
+    } else {
+        console.log("Nenhum arquivo selecionado")
+
+    }
+})
